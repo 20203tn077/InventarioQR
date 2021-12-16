059d2070e4a870d050659144c56784ea35d09408
20203tn096 <84577846+20203tn096@users.noreply.github.com>
2021-12-16 10:15:28 -0600

    Actualizacion de la base

--- Dump20211212.sql
+++ Dump20211212.sql
@@ -1,86 +1,53 @@
-CREATE DATABASE  IF NOT EXISTS `inventarioqr` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `inventarioqr`;
--- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
---
--- Host: localhost    Database: inventarioqr
--- ------------------------------------------------------
--- Server version	8.0.27
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `articulo`
 --
 
-DROP TABLE IF EXISTS `articulo`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `articulo` (
-  `codigo` bigint NOT NULL,
-  `nombre` varchar(50) NOT NULL,
-  `descripcion` text NOT NULL,
-  `categoria_id` int NOT NULL,
-  `cantidad` int NOT NULL
+DROP TABLE IF EXISTS articulo;
+CREATE TABLE articulo (
+  codigo bigint NOT NULL,
+  nombre varchar(50) NOT NULL,
+  descripcion text NOT NULL,
+  categoria_id int NOT NULL,
+  cantidad int NOT NULL,
+  PRIMARY KEY (codigo),
+  KEY articulo_categoria_FK (categoria_id),
+  CONSTRAINT articulo_categoria_FK FOREIGN KEY (categoria_id) REFERENCES categoria (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Dumping data for table `articulo`
 --
 
-LOCK TABLES `articulo` WRITE;
-/*!40000 ALTER TABLE `articulo` DISABLE KEYS */;
-INSERT INTO `articulo` VALUES (5546685421345,'PAPAS','Picosas',1,5),(7746685421345,'PAPAS','Picosas',1,5),(1234567891234,'Gomitas','Acidas',1,10);
-/*!40000 ALTER TABLE `articulo` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO articulo VALUES (1234567891235,'Gomitas','Acidas',1,5);
+INSERT INTO articulo VALUES (7746685421345,'PAPAS','Picosas',1,5);
+INSERT INTO articulo VALUES (7854155744151,'Mascarilla','Mascarilla de yogurt',1,2);
 
 --
 -- Table structure for table `categoria`
 --
 
-DROP TABLE IF EXISTS `categoria`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `categoria` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(40) NOT NULL,
-  PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS categoria;
+CREATE TABLE categoria (
+  id int NOT NULL AUTO_INCREMENT,
+  nombre varchar(40) NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Dumping data for table `categoria`
 --
 
-LOCK TABLES `categoria` WRITE;
-/*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
-INSERT INTO `categoria` VALUES (1,'Picosas');
-/*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO categoria VALUES (1,'Picosas');
 
 --
 -- Dumping routines for database 'inventarioqr'
 --
-/*!50003 DROP PROCEDURE IF EXISTS `insertar_articulo` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+
 DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `insertar_articulo`(
+CREATE DEFINER=root@localhost PROCEDURE insertar_articulo(
 	p_codigo varchar(13),
 	p_nombre varchar(50),
     p_descripcion text,
@@ -93,92 +60,56 @@ DECLARE verificarCodigo INT;
 SELECT COUNT(*) INTO verificarCodigo FROM articulo WHERE codigo = p_codigo; 
 IF verificarCodigo = 0 THEN 
 INSERT INTO articulo VALUES (p_codigo, p_nombre, p_descripcion, p_categoria, p_cantidad);
+SET p_errorArticulo = 0;
 ELSE 
 SET p_errorArticulo = 1;
 END IF;
 END ;;
 DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `pdo_consulta` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+ 
 DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `pdo_consulta`()
+CREATE DEFINER=root@localhost PROCEDURE pdo_consulta(
+Out p_count INT)
 BEGIN 
-SELECT * FROM articulo A INNER JOIN categoria C ON A.categoria_id = C.id;
+SET p_count = 0;
+SELECT COUNT(*) INTO p_count FROM articulo;
+SELECT A.codigo, A.nombre, A.descripcion, C.nombre, A.cantidad  FROM articulo A INNER JOIN categoria C ON A.categoria_id = C.id;
 END ;;
 DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `pdo_consultaCategoria` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+ 
 DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `pdo_consultaCategoria`()
+CREATE DEFINER=root@localhost PROCEDURE pdo_consultaCategoria(
+OUT p_count INT)
 BEGIN 
-SELECT * FROM categoria;
+SET p_count = 0;
+SELECT COUNT(*) INTO p_count FROM categoria;
+SELECT id, nombre FROM categoria;
 END ;;
 DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `pdo_consultaPorCategoria` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+
+
 DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `pdo_consultaPorCategoria`(
+CREATE DEFINER=root@localhost PROCEDURE pdo_consultaPorCategoria(
 p_categoria INT,
-OUT p_errorCategoria INT
+OUT p_errorCategoria INT,
+OUT p_count INT
 )
 BEGIN 
 DECLARE verificarCategoria INT;
+SET p_count = 0;
 SELECT COUNT(*) INTO verificarCategoria FROM categoria WHERE id = p_categoria; 
 IF verificarCategoria = 1 THEN 
-SELECT * FROM articulo A INNER JOIN categoria C ON A.categoria_id = C.id WHERE categoria_id = verificarCategoria;
+SELECT COUNT(*) INTO p_count FROM articulo WHERE categoria_id = verificarCategoria;
+SELECT  A.codigo, A.nombre, A.descripcion, C.nombre, A.cantidad  FROM articulo A INNER JOIN categoria C ON A.categoria_id = C.id WHERE categoria_id = verificarCategoria;
+SET p_errorCategoria = 0;
 ELSE 
 SET p_errorCategoria = 1;
 END IF;
 END ;;
 DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `pdo_consultaPorCodigo` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+
 DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `pdo_consultaPorCodigo`(
+CREATE DEFINER=root@localhost PROCEDURE pdo_consultaPorCodigo(
 p_codigo VARCHAR(13),
 OUT p_error INT   
 )
@@ -186,24 +117,11 @@ BEGIN
 DECLARE verificarCodigo INT;
 SELECT COUNT(*) INTO verificarCodigo FROM articulo WHERE codigo = p_codigo; 
 IF verificarCodigo = 1 THEN 
-SELECT * FROM articulo A INNER JOIN categoria C ON A.categoria_id = C.id WHERE codigo = p_codigo;
+SELECT A.codigo, A.nombre, A.descripcion, C.nombre, A.cantidad FROM articulo A INNER JOIN categoria C ON A.categoria_id = C.id WHERE codigo = p_codigo;
+SET p_error = 0;
 ELSE 
 SET p_error = 1;
 END IF;
 END ;;
 DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-12-12 16:18:31
