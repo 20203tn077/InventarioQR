b2f3e1945f0df0b91042a3e079edec83e8979843
20203tn096 <84577846+20203tn096@users.noreply.github.com>
2021-12-16 14:25:38 -0600

    Correccion de los procedimientos

--- Dump20211212.sql
+++ Dump20211212.sql
@@ -1,3 +1,13 @@
+--
+-- Table structure for table `categoria`
+--
+
+DROP TABLE IF EXISTS categoria;
+CREATE TABLE categoria (
+  id int NOT NULL AUTO_INCREMENT,
+  nombre varchar(40) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Table structure for table `articulo`
@@ -16,24 +26,11 @@ CREATE TABLE articulo (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
---
--- Dumping data for table `articulo`
---
 
-INSERT INTO articulo VALUES (1234567891235,'Gomitas','Acidas',1,5);
+
 INSERT INTO articulo VALUES (7746685421345,'PAPAS','Picosas',1,5);
 INSERT INTO articulo VALUES (7854155744151,'Mascarilla','Mascarilla de yogurt',1,2);
 
---
--- Table structure for table `categoria`
---
-
-DROP TABLE IF EXISTS categoria;
-CREATE TABLE categoria (
-  id int NOT NULL AUTO_INCREMENT,
-  nombre varchar(40) NOT NULL,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 
 --
@@ -45,7 +42,23 @@ INSERT INTO categoria VALUES (1,'Picosas');
 --
 -- Dumping routines for database 'inventarioqr'
 --
-
+ 
+DELIMITER ;;
+CREATE DEFINER=root@localhost PROCEDURE eliminar_articulo(
+p_codigo BIGINT,
+OUT p_error INT)
+BEGIN 
+DECLARE v_errorCodigo INT;
+SELECT COUNT(*) INTO  v_errorCodigo FROM articulo WHERE codigo = p_codigo;
+IF v_errorCodigo = 1 THEN
+DELETE FROM articulo WHERE  codigo = p_codigo;
+SET p_error = 1;
+ELSE
+SET p_error = 0;
+END IF;
+END ;;
+DELIMITER ;
+ 
 DELIMITER ;;
 CREATE DEFINER=root@localhost PROCEDURE insertar_articulo(
 	p_codigo varchar(13),
@@ -73,10 +86,10 @@ Out p_count INT)
 BEGIN 
 SET p_count = 0;
 SELECT COUNT(*) INTO p_count FROM articulo;
-SELECT A.codigo, A.nombre, A.descripcion, C.nombre, A.cantidad  FROM articulo A INNER JOIN categoria C ON A.categoria_id = C.id;
+SELECT A.codigo, A.nombre AS nombreArticulo, A.descripcion, C.nombre AS nombreCategoria, A.cantidad  FROM articulo A INNER JOIN categoria C ON A.categoria_id = C.id;
 END ;;
 DELIMITER ;
- 
+
 DELIMITER ;;
 CREATE DEFINER=root@localhost PROCEDURE pdo_consultaCategoria(
 OUT p_count INT)
@@ -86,8 +99,7 @@ SELECT COUNT(*) INTO p_count FROM categoria;
 SELECT id, nombre FROM categoria;
 END ;;
 DELIMITER ;
-
-
+ 
 DELIMITER ;;
 CREATE DEFINER=root@localhost PROCEDURE pdo_consultaPorCategoria(
 p_categoria INT,
@@ -100,14 +112,14 @@ SET p_count = 0;
 SELECT COUNT(*) INTO verificarCategoria FROM categoria WHERE id = p_categoria; 
 IF verificarCategoria = 1 THEN 
 SELECT COUNT(*) INTO p_count FROM articulo WHERE categoria_id = verificarCategoria;
-SELECT  A.codigo, A.nombre, A.descripcion, C.nombre, A.cantidad  FROM articulo A INNER JOIN categoria C ON A.categoria_id = C.id WHERE categoria_id = verificarCategoria;
+SELECT  A.codigo, A.nombre AS nombreArticulo, A.descripcion, C.nombre AS nombreCategoria, A.cantidad  FROM articulo A INNER JOIN categoria C ON A.categoria_id = C.id WHERE categoria_id = verificarCategoria;
 SET p_errorCategoria = 0;
 ELSE 
 SET p_errorCategoria = 1;
 END IF;
 END ;;
 DELIMITER ;
-
+ 
 DELIMITER ;;
 CREATE DEFINER=root@localhost PROCEDURE pdo_consultaPorCodigo(
 p_codigo VARCHAR(13),
@@ -117,11 +129,10 @@ BEGIN
 DECLARE verificarCodigo INT;
 SELECT COUNT(*) INTO verificarCodigo FROM articulo WHERE codigo = p_codigo; 
 IF verificarCodigo = 1 THEN 
-SELECT A.codigo, A.nombre, A.descripcion, C.nombre, A.cantidad FROM articulo A INNER JOIN categoria C ON A.categoria_id = C.id WHERE codigo = p_codigo;
+SELECT  A.codigo, A.nombre AS nombreArticulo, A.descripcion, C.nombre AS nombreCategoria, A.cantidad  FROM articulo A INNER JOIN categoria C ON A.categoria_id = C.id WHERE codigo = p_codigo;
 SET p_error = 0;
 ELSE 
 SET p_error = 1;
 END IF;
 END ;;
 DELIMITER ;
-
